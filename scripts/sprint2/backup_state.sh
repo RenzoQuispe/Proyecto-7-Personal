9ad9a9a8de75bbecf9e298743416c0d1bbbda651
RenzoQuispe <renzo123cd@gmail.com>
2025-06-15 20:03:53 -0500

    feat(script): actualizar script backup_state.sh con validacion de existencia de iac/terraform.tfstate

---
+++ scripts/sprint2/backup_state.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# se crea la carpeta backups si no existe
+ruta_a_este_script="$(realpath "$0")"
+ruta_carpeta_scripts="$(dirname "$ruta_a_este_script")"
+ruta_raiz_proyecto="$(dirname "$ruta_carpeta_scripts")"
+mkdir -p "$ruta_raiz_proyecto/backups"
+# creacion de una copia de terraform.tfstate a backups, se coloca timestamp para diferenciar entre backups
+timestamp="$(date +"%Y-%m-%d_%H-%M-%S")"
+ruta_terraform_state="$ruta_raiz_proyecto/iac/terraform.tfstate"
+# verificar que iac/terraform.tfstate existe
+if [ ! -f "$ruta_terraform_state" ]; then
+  echo "iac/terraform.tfstate no existe"
+  exit 1
+fi
+ruta_nuevo_backup="$ruta_raiz_proyecto/backups/tfstate_${timestamp}.backup"
+cp "$ruta_terraform_state" "$ruta_nuevo_backup"
